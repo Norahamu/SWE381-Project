0504626c4582dfdd08ca5f8145978328df6a5262
Nadaalotaibi1 <145347784+Nadaalotaibi1@users.noreply.github.com>
2024-05-03 21:16:48 +0300

    Update PartnerList.php

--- SWE381/PartnerList.php
+++ SWE381/PartnerList.php
@@ -62,7 +62,7 @@
 
             <div class ="superlanguage">
                 <?php
-                $connection = mysqli_connect('localhost', 'root', 'root', 'lingo');
+                $connection = mysqli_connect('localhost', 'root', '', 'lingo');
                 if (!$connection) {
                     die("Connection failed: " . mysqli_connect_error());
                 }
@@ -120,7 +120,7 @@
             <div class ="superlanguage">
 
                 <?php
-                $connection = mysqli_connect('localhost', 'root', 'root', 'lingo');
+                $connection = mysqli_connect('localhost', 'root', '', 'lingo');
                 if (!$connection) {
                     die("Connection failed: " . mysqli_connect_error());
                 }
@@ -170,7 +170,7 @@
             <h2 id ="FrenshLanguage" class =" LanguageTitle">French </h2>
             <div class ="superlanguage">
                 <?php
-                $connection = mysqli_connect('localhost', 'root', 'root', 'lingo');
+                $connection = mysqli_connect('localhost', 'root', '', 'lingo');
                 if (!$connection) {
                     die("Connection failed: " . mysqli_connect_error());
                 }
@@ -221,7 +221,7 @@
             <div class ="superlanguage">
 
                 <?php
-                $connection = mysqli_connect('localhost', 'root', 'root', 'lingo');
+                $connection = mysqli_connect('localhost', 'root', '', 'lingo');
                 if (!$connection) {
                     die("Connection failed: " . mysqli_connect_error());
                 }
